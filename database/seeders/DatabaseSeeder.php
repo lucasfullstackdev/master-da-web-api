dd632cd48a5047f76359a3fe98955b83550ca80a
lucas oliveira <lucas.fullstack.dev@gmail.com>
2023-06-19 09:36:33 -0300

    feat: criando clientes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ProductSeed::class
+            ProductSeed::class,
+            UserSeed::class
         ]);
     }
 }
